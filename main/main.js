5f300c20aad3ea6e09fac3a3101c7dd73f3bbc2e
jaemee <55077337+jaemee@users.noreply.github.com>
2019-10-07 19:53:51 +0800

    refactor codes and functions

--- main/main.js
+++ main/main.js
@@ -5,67 +5,62 @@ module.exports = function main(inputs) {
 };
 
 function printReceipt(inputs){
-    let receiptText = '***<store earning no money>Receipt ***\n';
-    var name = '';
-    var unit = '';
-    var singlePrice = 0;
-    var price = 0;
-    var quantity = 0;
-    var counter = 1;
-    var total = 0;
-    
-    
-   inputs.forEach(item => {
-       if(counter === 1){
-            setItemInfos(item);
-            quantity++;
-       }else if (name === item.Name){
-            price = price + item.Price;
-            quantity++;
-       }else if(name !== item.Name && counter < inputs.length){
-            unit = setUnitName(quantity, unit)
-            receiptText += createReceipt(name, quantity, unit, singlePrice, price);
-            setItemInfos(item);
-            quantity = 1;
-       }
-       if (counter === inputs.length){
-            unit = setUnitName(quantity, unit)
-            receiptText += createReceipt(name, quantity, unit, singlePrice, price);
-            setItemInfos(item);
-
-            quantity = 1;
-            total = total + singlePrice;
-            unit = setUnitName(quantity, unit)
-            receiptText += createReceipt(name, quantity, unit, singlePrice, price);
-            
-            receiptText = receiptText + '----------------------\n' + 
-                'Total: ' + total.toFixed(2) + ' (yuan)\n' + 
-                '**********************\n';
-        }
-        total = total + singlePrice;
-        counter++;
-   });
-
-   function setItemInfos(item){
-    name = item.Name;
-    unit = item.Unit;
-    price = item.Price;
-    singlePrice = item.Price;
-   };
-
-   return receiptText;
+    const barCodes = [...new Set(inputs.map(input => input.Barcode))];
+    var itemList = processData(inputs, barCodes);
+   return createReceipt(itemList);
 };
 
-   function setUnitName(quantity, unit){
-    var unitName = '';
+function processData(inputs, barCodes){
+    var itemList = [];
+    barCodes.forEach(barCode => {
+        var item = {};
+        var itemName = inputs.filter(input => input.Barcode === barCode)
+                            .map(input => input.Name)[0];
+        var itemPrice = inputs.filter(input => input.Barcode === barCode)
+                            .map(input => input.Price)[0];   
+        var itemUnit = inputs.filter(input => input.Barcode === barCode)
+                            .map(input => input.Unit)[0];  
+        var totalPricePerItem = inputs.filter(input => input.Barcode === barCode)
+                                .map(input => input.Price)
+                                .reduce((price, totalPrice) => price + totalPrice, 0);
+        var quantity = inputs.filter(input => input.Barcode === barCode)
+                                .reduce((totalQuantity) => totalQuantity+1, 0);
+
+        item = {
+            Barcode: barCode,
+            ItemName: itemName,
+            ItemPrice: itemPrice,
+            TotalPrice: totalPricePerItem,
+            Quantity: quantity,
+            Unit: itemUnit
+        }
+        itemList.push(item);
+    });
+    return itemList;
+}
+
+function createReceipt(itemList){
+    let receiptText = '***<store earning no money>Receipt ***\n';
+    itemList.forEach(item => {
+      receiptText += 'Name: ' + item.ItemName + ', Quantity: ' + item.Quantity + '' + setUnitName(item.Quantity, item.Unit) + ', Unit price: ' + item.ItemPrice.toFixed(2) + ' (yuan), Subtotal: ' + item.TotalPrice.toFixed(2) + ' (yuan)\n';
+    })
+
+    var totalPrice = itemList.map(item => item.TotalPrice)
+                         .reduce((price, totalPrice) => price + totalPrice, 0);
+
+    receiptText += '----------------------\n' + 
+             'Total: ' + totalPrice.toFixed(2) + ' (yuan)\n' + 
+             '**********************\n';
+    return receiptText;
+};
+
+function setUnitName(quantity, unit){
     if(unit === 'a'){
         return '';
     }
     if(quantity > 1){
         return ' ' + unit + 's';
     }
-   };
+};
 
-   function createReceipt(name, quantity, unit, singlePrice, price){
-        return 'Name: ' + name + ', Quantity: ' + quantity + '' + unit + ', Unit price: ' + singlePrice.toFixed(2) + ' (yuan), Subtotal: ' + price.toFixed(2) + ' (yuan)\n';
-   };
+
